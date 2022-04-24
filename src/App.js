e773d99413afbc9c478365036cf74802025fe35e
sahar <parvaresh.sahar7171@gmail.com>
2022-04-24 22:33:22 +0430

    added my file

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import React from "react";
+import CustomForm from "./Components/CustomFormik/Form/CustomForm";
+
+function App() {
+  return (
+    <>
+      <CustomForm />
+    </>
+  );
+}
+
+export default App;
